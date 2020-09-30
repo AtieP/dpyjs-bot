427ea1667979d4faf8a784d796f17dc732cedbe7
sadie.lieder <sadie.lieder@gmail.com>
2020-09-29 20:21:04 -0400

    Remove DurationConverter.__init__
    MissingRequiredArgument is thrown if the duration isn't specified, which makes this change entirely useless in a converter.

--- bot/converters.py
+++ bot/converters.py
@@ -1,5 +1,5 @@
 import re
-from datetime import datetime, timedelta
+from datetime import datetime
 from typing import Optional
 
 import dateutil.parser
@@ -45,16 +45,6 @@ class DurationConverter(commands.Converter):
         r"(?:(?P<seconds>\d{1,5})s)?"
     )
 
-    def __init__(self, *, default: Optional[timedelta] = None):
-        """
-        Initialises the duration converter.
-
-        The optional `default` keyword argument is a `datetime.timedelta`
-        instance that represents the default duration span to return in
-        case an invalid duration string was provided by a user.
-        """
-        self.default = default
-
     async def convert(
         self,
         ctx: commands.Context,
@@ -64,18 +54,12 @@ class DurationConverter(commands.Converter):
         Converts a duration string into a `datetime.datetime` object.
 
         Example of a duration string: 1h2m3s
-
-        If an invalid duration string was provided and a default timedelta
-        was specified, the default timedelta will be used instead.
         """
         match = self.compiled.fullmatch(duration_str)
         if match is None or not match.group(0):
-            if self.default is not None:
-                return datetime.now() + self.default
-            else:
-                raise commands.BadArgument(
-                    f'Invalid duration string provided: "{duration_str}"'
-                )
+            raise commands.BadArgument(
+                f'Invalid duration provided: "{duration_str}"'
+            )
 
         duration_dict = {
             k: int(v)
